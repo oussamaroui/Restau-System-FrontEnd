ed3693c1090a8e27c1b081cc0ab59bd335b0efc8
oussamaroui <oussamaroui9@gmail.com>
2024-05-07 03:22:08 +0200

    Add style for category click

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ window.addEventListener('scroll', function () {
     var headerText = document.querySelector('header p');
     var scrollY = window.scrollY || window.pageYOffset;
 
-    // Calculate alpha for background color change
-    var alpha = Math.min(scrollY / 165, 1);
-    headerDiv.style.backgroundColor = 'rgba(244, 110, 0, ' + alpha + ')';
-    if (scrollY == 0 ) headerDiv.style.backgroundColor = 'rgba(0, 0, 0, 0.311)'
-
     // Calculate font size change
     var fontSize = 26 - (scrollY / 20);
     fontSize = Math.max(fontSize, 18);
     headerText.style.fontSize = fontSize + 'px';
+});
+
+var categories = document.querySelectorAll('#category div');
+
+categories.forEach(function (category) {
+    category.addEventListener('click', function () {
+        if (category.getAttribute('style') == "background-color: rgba(244, 110, 0, 0.3); border: 1px solid rgb(244, 110, 0);") {
+            category.style.backgroundColor = "white";
+            category.style.border = "1px solid white";
+        } else {
+            categories.forEach(function (otherCategory) {
+                otherCategory.style.backgroundColor = "";
+                otherCategory.style.border = "";
+            });
+            category.style.backgroundColor = "rgba(244, 110, 0, 0.3)";
+            category.style.border = "1px solid rgba(244, 110, 0)";
+        }
+    });
 });
